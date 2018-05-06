3c74e24a6e9ce29cdc35507cebe01558e90a37da
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-06 14:55:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.tolowerCase() === lowercase)
+  if (string.toLowerCase() === lowercase)
   return "I can't hear you!"
 }
